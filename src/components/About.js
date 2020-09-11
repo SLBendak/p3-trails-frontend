9ab5106bacd1a939311f3e7694dd081bad9a770e
mac <shebah712@gmail.com>
2020-09-11 11:01:44 -0700

    [about] update about message

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div className="welcomeDiv">
         <h1>About</h1>
         <p>this is all about us</p>
-        <p>For instance, on the planet Earth, man had always assumed that he was more intelligent than dolphins because he had achieved so much—the wheel, New York, wars and so on—whilst all the dolphins had ever done was muck about in the water having a good time. But conversely, the dolphins had always believed that they were far more intelligent than man—for precisely the same reasons.</p>
+        <p>Trail finder is a convenient way to locate trails and hikes near you. On a vacation, in a new town, or just trying to find new places to hike. Trail finder is the answer. Save all your favorite trails or save trails you want to explore one day. </p>
     </div>
 
     );
