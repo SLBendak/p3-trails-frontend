47f79384e8f7ecb3c98c0eb7ac17be76d82b2b1f
mac <shebah712@gmail.com>
2020-09-11 11:04:43 -0700

    about update

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,6 @@ const About = () => {
     return(
     <div className="welcomeDiv">
         <h1>About</h1>
-        <p>this is all about us</p>
         <p>Trail finder is a convenient way to locate trails and hikes near you. On a vacation, in a new town, or just trying to find new places to hike. Trail finder is the answer. Save all your favorite trails or save trails you want to explore one day. </p>
     </div>
 
